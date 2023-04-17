ef543f606a5e2098b40ccfc3788ca8c7b5ea2c26
Hemant <92645502+hemantkumar76@users.noreply.github.com>
2023-04-17 22:50:51 +0530

    Update util.cpp

--- core/src/main/c/share/util.cpp
+++ core/src/main/c/share/util.cpp
@@ -1,5 +1,7 @@
+// hi, I am hemant and i just changed this code and optimized this C code according to my knowledge as i am BTECH-Student (end year) accoridng to my knowledge i did it i hope thats works for the swag things :)
+
 #include <jni.h>
-#include <cstdint>
+#include <stdint.h>
 #include "asmlib/asmlib.h"
 
 extern "C" {
@@ -7,19 +9,25 @@ extern "C" {
 #ifdef __APPLE__
 JNIEXPORT jlong JNICALL Java_io_questdb_std_Os_compareAndSwap
         (JNIEnv *e, jclass cl, jlong volatile ptr, jlong oldVal, jlong newVal) {
-    return __sync_val_compare_and_swap(
+    return __atomic_compare_exchange_n(
             reinterpret_cast<int64_t *>(ptr),
-            (int64_t) (oldVal),
-            (int64_t) (newVal)
+            &oldVal,
+            newVal,
+            false,
+            __ATOMIC_SEQ_CST,
+            __ATOMIC_SEQ_CST
     );
 }
 #else
 JNIEXPORT jlong JNICALL Java_io_questdb_std_Os_compareAndSwap
         (JNIEnv *e, jclass cl, jlong volatile ptr, jlong oldVal, jlong newVal) {
-    return __sync_val_compare_and_swap(
+    return __atomic_compare_exchange_n(
             reinterpret_cast<int64_t *>(ptr),
-            reinterpret_cast<int64_t>(oldVal),
-            reinterpret_cast<int64_t>(newVal)
+            &oldVal,
+            newVal,
+            false,
+            __ATOMIC_SEQ_CST,
+            __ATOMIC_SEQ_CST
     );
 }
 #endif
